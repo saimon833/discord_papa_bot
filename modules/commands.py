de95d840110dc4886903caad2036cd89119bd5ba
saimon833 <74502684+saimon833@users.noreply.github.com>
2021-01-01 02:06:33 +0100

    Update commands.py

--- modules/commands.py
+++ modules/commands.py
@@ -2,23 +2,24 @@ from discord.ext.commands.core import check
 from discord.utils import get
 from dotenv import load_dotenv
 
-import modules.functions as f
+from modules.functions import *
+
 
 async def play(ctx,client,ranks,main_channel):
-    if await f.check_permission(ctx,ranks)==False:
+    if await check_permission(ctx,ranks)==False:
         return
     ogolny=client.get_channel(main_channel) 
     voice_ch=ctx.author.voice.channel
     await ogolny.send('Kremówka time!')
     await ogolny.send('https://piekarniagrzybki.pl/wp-content/uploads/2017/12/kremowka.jpg')
-    await f.join_papa(voice_ch,'comm',client)
+    await join_papa(voice_ch,'comm',client)
 
 async def leave(ctx,ranks):
-    if await f.check_permission(ctx,ranks)==False:
+    if await check_permission(ctx,ranks)==False:
         return
     await ctx.voice_client.disconnect()
 
-async def seppuku(ctx): #stolen from SKNI KOD bot
+async def seppuku(ctx):
     reason='Popełnił sekkupu. Gloria Victis!'
     await ctx.author.kick(reason=reason)
     tmp=str(ctx.message.author)+' popełnij seppuku'
@@ -26,27 +27,24 @@ async def seppuku(ctx): #stolen from SKNI KOD bot
     await ctx.send('https://media1.tenor.com/images/6f64764b4b7874465d83de68342347cc/tenor.gif')
 
 async def mute(ctx, person,ranks,muted):
-    if ctx.message.author.id == f.set_ID(person):
-        await ctx.send("Nie możesz zmutować siebie, głuptasku")
+    if await check_permission(ctx,ranks)==False:
         return
-    if await f.check_permission(ctx,ranks)==False:
-        return
-    if f.set_ID(person) in muted:
+    if set_ID(person) in muted:
         await ctx.send("Podany user jest zmutowany lub nie istnieje")
     else:
-        muted.add(f.set_ID(person))
+        muted.add(set_ID(person))
 
 async def unmute(ctx, person,ranks,muted):
-    if await f.check_permission(ctx,ranks)==False:
+    if await check_permission(ctx,ranks)==False:
         return
     try:
-        muted.remove(f.set_ID(person))
+        muted.remove(set_ID(person))
     except:
         await ctx.send("Podany user jest odmutowany lub nie istnieje")
 
 async def clear(ctx, amount, client, ranks):
     channel = ctx.message.channel
-    if await f.check_permission(ctx,ranks)==False:
+    if await check_permission(ctx,ranks)==False:
         return
     if amount>20:
         await ctx.send('Maks 20 wiadomości')
